ab7f3905cd2b145603976d3a3add8c0ea9c3a40e
marisareyes24 <marisa.reyes.a@gmail.com>
2024-04-14 18:17:33 -0500

    proyecto final

--- VehicleStore/settings.py
+++ VehicleStore/settings.py
@@ -38,15 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'vehicles',
+    'drf_yasg',
     'coreapi',
+    'rest_framework',    
+    'corsheaders',    
+    'vehicles',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',    
     'django.middleware.csrf.CsrfViewMiddleware',  #verifica si hacen ataque 
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -135,3 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
--- vehicles/models.py
+++ vehicles/models.py
@@ -8,6 +8,5 @@ class Vehicle(models.Model):
     year = models.IntegerField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     image = models.ImageField(upload_to='vehicle_images/')
-
-def __str__(self):
-    return self.name + ' ' + self.model
+    def __str__(self):
+        return self.name + ' ' + self.model
